2b2b40fcc906a7f1a4b7578e361ac48f5ed42dd9
SAEZ Jonathan <saez.jonathan@gmail.com>
2013-04-09 08:56:37 +0200

    Mise en forme de la classe déception personnalisé
    Dérivé de la classe exception
    ---> mise en place quasi sure d'un logger (choix a faire)
    ---> message pré-formaté en détail
    ---> mise en place en cour : gestion des exception orpheline

--- system/CoreException.class.php
+++ system/CoreException.class.php
@@ -28,6 +28,34 @@
 require_once "defineConstant.inc.php";
 class CoreException extend Exception{
 
+    protected $eMessage;
+    protected $eCode;
+    protected $eFile;
+    protected $eLine;
+    protected $timestamp;
+
+    // constructeur enrichie de la classe exception
+    public function __construct($eMessage , $eCode = 0) {
+        parent::__construct($eMessage, $eCode);
+        $this->timestamp=time();
+
+
+        //to do --> systeme de log
+        // utilisation de syslog ou une classe de log ?
+        
+    }
+
+    // Retourne un message en forme de l'exception
+    public getMessageFormate()
+    {
+        $messageFormate = "Mon message mis en forme";
+        return $messageFormate;
+    }
+
+    public static gestionExceptionOrpheline()
+    {
+        set_exception_handler('Nom de la fonction de gestion');  // to do
+    } 
     
 }
 
