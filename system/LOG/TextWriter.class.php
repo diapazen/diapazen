382b05049f1326dd66cf877f68d8b7aca6d8cc48
SAEZ Jonathan <saez.jonathan@gmail.com>
2013-04-10 11:41:12 +0200

    Mise en place de la structure du writer text

--- system/LOG/TextWriter.class.php
+++ system/LOG/TextWriter.class.php
@@ -23,10 +23,37 @@
  * along with Diapazen.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
+require_once "IWrite.php";
+require_once "../defineConstant.inc.php";
 class TextWriter implements IWrite
 {
-   private const $logsize=5242880; //5Mo
+   private const $logSizeMax=5242880; //5Mo
+   private const $logSizeAlert=5138020; // environs 98% de 5Mo
+   private $path =WRITER_ROOT."log.txt";
+
+
+   public function write($message,$level,$date)
+   {
+
+   }
+
+   private function openFile()
+   {
+
+   }
+   private function closeFile()
+   {
+
+   }
+   private function testSize()
+   {
+
+   }
+   private function addLine($text)
+   {
+
+   }
+
 
 }
 
