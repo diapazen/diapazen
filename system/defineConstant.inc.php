f6850ff9a2b8e88a42cd169a780669629fc4c0ef
Timothée Nicolas <timothnicolas@gmail.com>
2013-04-26 18:50:46 +0200

    Modification des constantes

--- system/defineConstant.inc.php
+++ system/defineConstant.inc.php
@@ -31,15 +31,17 @@ define("DS", DIRECTORY_SEPARATOR);
 define("ROOT", dirname(__FILE__).DS.'..'.DS);
 define("SYSTEM_ROOT", dirname(__FILE__).DS);
 define("APP_ROOT", ROOT.'app'.DS);
-define("MODEL_ROOT", APP_ROOT.'model'.DS);
+//define("MODEL_ROOT", APP_ROOT.'model'.DS);
 //define("VIEW_ROOT",APP_ROOT.'view'.DS);
 //define("CONTROLLER_ROOT", APP_ROOT.'controller'.DS);
-//define("UTIL_ROOT", ROOT.'util'.DS);
-//define("WRITER_ROOT", SYSTEM_ROOT.DS.'LOG'.DS);
+define("UTIL_ROOT", ROOT.'util'.DS);
+define("WRITER_ROOT", SYSTEM_ROOT.DS.'LOG'.DS);
 define('LOADER', SYSTEM_ROOT.DS.'CoreLoader.class.php');
 
 
 define('BASE_URL', dirname($_SERVER['SCRIPT_NAME']));
-define('VIEW_ROOT', BASE_URL.DS.'app'.DS.'view'.DS);
+define('VIEW_ROOT', DS.'app'.DS.'view');
+define('CONTROLLER_ROOT', DS.'app'.DS.'controller');
+define('MODEL_ROOT', DS.'app'.DS.'model');
 
 ?>
