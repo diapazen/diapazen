937027ddd9564f14356415cae93ffacd95f7293f
Timothée Nicolas <timothnicolas@gmail.com>
2013-04-27 16:02:32 +0200

    Ajout de la page d'erreur 404

---
+++ app/view/php/404.php
@@ -0,0 +1,13 @@
+
+
+<?php $this->getHeader(); ?>
+        
+        <div id="content">
+            
+            <h1>Erreur 404</h1>
+            <h2>La page demandée n'existe pas.</h2>
+            
+        </div>
+
+<?php $this->getFooter(); ?>
+        
