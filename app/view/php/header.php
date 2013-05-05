05742bfccdfe225746facd5ee0e1efd79aaf2d75
Timothée Nicolas <timothnicolas@gmail.com>
2013-05-05 23:28:37 +0200

    Correction d'un bug dans le form. de connexion

--- app/view/php/header.php
+++ app/view/php/header.php
@@ -18,7 +18,7 @@
 				</a>
 
 				<?php if (!$this->isUserConnected()){ ?>
-				    	<form action="user/login" id="connect_box" method="post"><!-- <h3 class="small_title">Se connecter</h3> -->
+				    	<form action="<?php $this->getHomeUrl(); ?>/user/login" id="connect_box" method="post"><!-- <h3 class="small_title">Se connecter</h3> -->
 				        	<input id="mail_connect" name="mailConnect" class="small_text_edit" type="mail" placeholder="E-mail">
 				        	<input id="password_connect" name="passwordConnect" class="small_text_edit" type="password" placeholder="Mot de passe">
 				        	<input class ="orange_small_button" type="submit" value="Connexion">
