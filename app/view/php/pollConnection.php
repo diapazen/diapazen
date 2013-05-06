78bd2671f7c64d8784a2f03d62b9345f0a688a6d
Disch anthony <anthony.disch@isen.fr>
2013-05-06 17:40:28 +0200

    Ajout name sur les champs email/password du poll creation

--- app/view/php/pollConnection.php
+++ app/view/php/pollConnection.php
@@ -7,12 +7,12 @@
                 <a class="grey_ariadne" href="#">Partage</a>
             </div>
             <form action="share" method="post" id="poll_connection_form">
-                <input id="mail_user" class="small_text_edit" type="mail" placeholder="E-mail">
+                <input id="mail_user" class="small_text_edit" name="email" type="mail" placeholder="E-mail">
                 <label class="text"><input id="registered" type="radio" name="account">Déjà un compte</label>
                 <label class="text"><input id="not_registered" type="radio" name="account">C'est rapide, je m'inscris</label>
                 <input class="small_text_edit" id="name_user" name="nameUser" type="text" placeholder="Nom">
                 <input class="small_text_edit" id="first_name_user" name="firstNameUser" type="text" placeholder="Prénom">
-                <input id="pwd_user" class="small_text_edit" type="password" placeholder="Mot de passe">
+                <input id="pwd_user" class="small_text_edit" type="password" name="password" placeholder="Mot de passe">
                 <p id="mail_info" class="text">Un mail sera envoyé sur l'adresse e-mail entrée ci-dessus avec votre mot de passe généré aléatoirement</p>
                 <input class="orange_button" type="submit" value="Etape suivante"/>
             </form>
