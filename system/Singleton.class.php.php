dfbd64724e948e60d459018524cd271dea8f70a3
SAEZ Jonathan <saez.jonathan@gmail.com>
2013-05-06 12:27:20 +0200

    Mise en forme singleton
    Utilise les reflexion class

--- system/Singleton.class.php.php
+++ system/Singleton.class.php.php
@@ -1,5 +1,29 @@
 <?php
 
+/**
+ * 
+ * Fichier de définitions des constantes
+ * 
+ * @package     Diapazen
+ * @copyright   Copyright (c) 2013, ISEN-Toulon
+ * @license     http://www.gnu.org/licenses/gpl.html GNU GPL v3
+ * 
+ * This file is part of Diapazen.
+ * 
+ * Diapazen is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License 3 as published by
+ * the Free Software Foundation.
+ * 
+ * Diapazen is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Diapazen.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 class coreSingleton {
     protected static $_instances; //tableau des instance singleton
     public static $namespace = __NAMESPACE__;
