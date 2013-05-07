3d75a5608b9330b4a799d882f0e092dd489ad09e
Julien Bodet <julien.bodet92@gmail.com>
2013-05-07 20:22:48 +0200

    Mise à jour login.php

--- app/view/php/login.php
+++ app/view/php/login.php
@@ -8,9 +8,9 @@
             
             <form action="" id="connect_box_fail">
             	<p class="title" >Réessayer de vous connecter !</p>
-	        	<input id="mail_connect_fail" name="mailConnect" class="small_text_edit" type="mail" placeholder="E-mail">
-	        	<input id="password_connect_fail" name="passwordConnect" class="small_text_edit" type="password" placeholder="Mot de passe">
-	        	<input class ="orange_small_button" type="submit" value="Connexion">
+	        	<input id="mail_connect_fail" name="mailConnect" class="text_edit" type="mail" placeholder="E-mail">
+	        	<input id="password_connect_fail" name="passwordConnect" class="text_edit" type="password" placeholder="Mot de passe">
+	        	<input class ="orange_button" type="submit" value="Connexion">
             </form>
         </div>
 
