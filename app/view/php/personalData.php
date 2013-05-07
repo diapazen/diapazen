06c469094bbe1540a9c35e0aaabb4f5fba2f675d
Timothée Nicolas <timothnicolas@gmail.com>
2013-05-07 18:51:17 +0200

    Modification de la vue du profil

--- app/view/php/personalData.php
+++ app/view/php/personalData.php
@@ -11,8 +11,11 @@
                 <input type="text" class="text_edit" value="<?php if(isset($lastname)){echo $lastname;}?>" placeholder="Nom" name="lastname">
                 <input type="text" class="text_edit" value="<?php if(isset($firstname)){echo $firstname;}?>" placeholder="Prénom" name="firstname">
                 <input id="user_mail" type="mail" class="text_edit" value="<?php if(isset($email)){echo $email;}?>" placeholder="E-mail" name="mail">
-                <input type="password" class="text_edit" value="" placeholder="Mot de passe" name="password">
+                <input type="password" class="text_edit" value="" placeholder="Nouveau mot de passe" name="password">
                 <input type="password" class="text_edit" value="" placeholder="Confirmer le mot de passe" name="passwordConfirm">
+                <br/>
+                <p>Pour confirmer les modification, entrer votre mot de passe actuel.</p>
+                <input type="password" class="text_edit" value="" placeholder="Mot de passe" name="passwordSecurity">
                 <input class="orange_button" type="submit" value="Modifier">
             </form>    
         </div>
