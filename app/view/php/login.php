87dfa4afe09bb68ab0ff70af6047bc5a2019c278
Julien Bodet <julien.bodet92@gmail.com>
2013-05-07 20:04:38 +0200

    Page de login + corrections de bugs style.css + créations de box d'erreur et de succés

--- app/view/php/login.php
+++ app/view/php/login.php
@@ -4,12 +4,13 @@
         
         <div id="content">
             
-            <h1>Erreur de connexion</h1>
-            <h2>Le nom d'utilisateur ou le mot de passe est incorrect.</h2>
-            <form action="" id="connect_box">
-	        <input id="mail_connect" name="mailConnect" class="small_text_edit" type="mail" placeholder="E-mail">
-	        <input id="password_connect" name="passwordConnect" class="small_text_edit" type="password" placeholder="Mot de passe">
-	        <input class ="orange_small_button" type="submit" value="Connexion">
+            <div class="error_message" >Erreur de connexion, le nom d'utilisateur ou le mot de passe est incorrect.</div>
+            
+            <form action="" id="connect_box_fail">
+            	<p class="title" >Réessayer de vous connecter !</p>
+	        	<input id="mail_connect_fail" name="mailConnect" class="small_text_edit" type="mail" placeholder="E-mail">
+	        	<input id="password_connect_fail" name="passwordConnect" class="small_text_edit" type="password" placeholder="Mot de passe">
+	        	<input class ="orange_small_button" type="submit" value="Connexion">
             </form>
         </div>
 
