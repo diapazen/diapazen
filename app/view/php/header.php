ad9c388139ee78b286fb2cfcac25ef65a32e67e8
Timothée Nicolas <timothnicolas@gmail.com>
2013-05-07 13:15:30 +0200

    Ajout de jQuery 2.0

--- app/view/php/header.php
+++ app/view/php/header.php
@@ -6,6 +6,9 @@
         <link rel="stylesheet" type="text/css" href="<?php $this->getPath('css/orangeSoberKit.css'); ?>">
         <link rel="stylesheet" type="text/css" href="<?php $this->getPath('css/diapazen.css'); ?>">
         <link rel="stylesheet" type="text/css" href="<?php $this->getPath('css/style.css'); ?>">
+        <!-- jQuery 2.0 -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
         <title><?php if (isset($title)){ echo $title; } ?></title>
     </head>
 
