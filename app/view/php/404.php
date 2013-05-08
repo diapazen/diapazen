3d8349e0dfc063d81eeda0b1476447ef1d50ea31
Julien Bodet <julien.bodet92@gmail.com>
2013-05-08 12:49:07 +0200

    Mise à jour de la page 404

--- app/view/php/404.php
+++ app/view/php/404.php
@@ -3,10 +3,12 @@
 <?php $this->getHeader(); ?>
         
         <div id="content">
-            
-            <h1>Erreur 404</h1>
-            <h2>La page demandée n'existe pas.</h2>
-            
+            <img id="broken_diapason" src="<?php $this->getPath('media/pictures/diapason.png'); ?>" alt="diapason" >
+            <div id="text_404">
+            	<p class="big_title" >Erreur 404</p>
+            	<p class="title" >La page demandée n'existe pas.</p>
+            	<a class="link" href="<?php $this->getHomeUrl(); ?>">Aller à la page d'accueil</a>
+            </div>
         </div>
 
 <?php $this->getFooter(); ?>
