0e4866ad1069698eaf2e638de6e0856c3d81b44c
Guillaume Bauduin <guillaumebauduin92@gmail.com>
2013-05-09 18:12:33 +0200

    Correction lien fichier js

--- app/view/php/pollConnection.php
+++ app/view/php/pollConnection.php
@@ -10,9 +10,9 @@
 
                 <input id="mail_user" class="small_text_edit" name="email" type="email" placeholder="E-mail">
 
-                <label class="text"><input value="registered" type="radio" name="account">Déjà un compte</label>
+                <label class="text"><input id="registered" type="radio" name="account">Déjà un compte</label>
 
-                <label class="text"><input value="not_registered" type="radio" name="account">C'est rapide, je m'inscris</label>
+                <label class="text"><input id="not_registered" type="radio" name="account">C'est rapide, je m'inscris</label>
 
                 <input class="small_text_edit" id="name_user" name="nameUser" type="text" placeholder="Nom">
 
@@ -21,9 +21,10 @@
                 <input id="pwd_user" class="small_text_edit" type="password" name="password" placeholder="Mot de passe" value="">
 
                 <p id="mail_info" class="text">Un mail sera envoyé sur l'adresse e-mail entrée ci-dessus avec votre mot de passe généré aléatoirement</p>
-                
+
                 <input class="orange_button" type="submit" value="Etape suivante"/>
             </form>
         </div>
-        <script type="text/javascript" src="../js/pollConnection.js"></script>
+        <script type="text/javascript" src="<?php $this->getPath('js/pollConnection.js'); ?>"></script>
 <?php $this->getFooter(); ?>
+
