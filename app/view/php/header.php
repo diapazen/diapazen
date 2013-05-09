d9b835e58526292d32f89be07f68500741b7b0cd
Julien Bodet <julien.bodet92@gmail.com>
2013-05-09 09:47:16 +0200

    Ajout du lien 'Mot de passe oublié ?'

--- app/view/php/header.php
+++ app/view/php/header.php
@@ -21,11 +21,14 @@
 				</a>
 
 				<?php if (!$this->isUserConnected()){ ?>
-				    	<form action="<?php $this->getHomeUrl(); ?>/user/login" id="connect_box" method="post"><!-- <h3 class="small_title">Se connecter</h3> -->
+					<div id="connect_box" >
+				    	<form action="<?php $this->getHomeUrl(); ?>/user/login" method="post"><!-- <h3 class="small_title">Se connecter</h3> -->
 				        	<input id="mail_connect" name="mailConnect" class="small_text_edit" type="mail" placeholder="E-mail">
 				        	<input id="password_connect" name="passwordConnect" class="small_text_edit" type="password" placeholder="Mot de passe">
 				        	<input class ="orange_small_button" type="submit" value="Connexion">
 						</form>
+						<a class="small_link">Mot de passe oublié ?</a>
+					</div>
 				<?php } else { ?>
 						<div id="connected_box" ><p class="text" >Bonjour <span class="orange_text"><?php echo $this->getUserInfo('firstname').' '.$this->getUserInfo('lastname').' '; ?></span></p><a class="link" href="<?php $this->getHomeUrl(); ?>/user/logout">Se déconnecter</a></div>
 				<?php } ?>
