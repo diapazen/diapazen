88373fdff6f43d252b7bdc65106515809a1b40a9
Julien Bodet <julien.bodet92@gmail.com>
2013-05-13 00:12:42 +0200

    Modifications Vue PollShare.php

--- app/view/php/pollShare.php
+++ app/view/php/pollShare.php
@@ -6,15 +6,13 @@
                 <a class="grey_ariadne" href="#"><span></span><span>Connexion</span><span></span></a>
                 <a class="orange_ariadne" href="#"><span></span><span>Partage</span><span></span></a>
             </div>
-            <form id="share_form" methode="post" action="share">
-                <h1 class="big_title">Félicitations !</h1>
-                <h2 class="title">Votre sondage a été créé !</h2>
-                <h3 class="small_title">Votre lien :</h3>
-                <input type="text" onClick="this.select();" value="http://www.diapazen.com/poll/454d65f4" />
-                <!-- <a class="link" id="link_output" href="pollView.php">http://www.diapazen.com/poll/454d65f4</a> -->
-                <h3 class="small_title">Partagez :</h3>
-                <p class="text" id="share_info">Le lien ci-dessus leur sera envoyé</p>
-                <textarea class="small_text_edit" placeholder="mail.example@mail.com"></textarea>
+            <form id="share_form" class="default_form" method="post" action="share">
+                <h2 class="title">Votre sondage a bien été créé !</h2>
+                <h3 class="small_title">Lien du sondage</h3>
+                <p class="orange_text" >Copier ce lien pour le donner à qui vous souhaitez.</p>
+                <input type="text" class="text_edit" onClick="this.select();" value="http://www.diapazen.com/poll/454d65f4" />
+                <h3 class="small_title">Partagez ce lien par e-mail</h3>
+                <textarea class="text_edit" placeholder="mail.example@mail.com"></textarea>
                 <input class="orange_button" type="submit" value="Partager">
             </form>
         </div>
