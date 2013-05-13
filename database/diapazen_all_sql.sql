2b0d99327f30d7be32bd1b81b321ba88e0a046ac
Disch anthony <anthony.disch@isen.fr>
2013-05-13 20:43:51 +0200

    SQL : plus de données
    11 membres des CIR3
    Plus de sondages, avec plus de choix et plus de votants

--- database/diapazen_all_sql.sql
+++ database/diapazen_all_sql.sql
@@ -92,12 +92,6 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 
 
-
-
-
-
-
-
 /*-----------------------------------------------------------------------------*/
 /*----------------------------------Diapazen-----------------------------------*/
 /*-----------------------------------Views-------------------------------------*/
@@ -147,11 +141,6 @@ ORDER BY USER_ID ASC;
 
 
 
-
-
-
-
-
 /*-----------------------------------------------------------------------------*/
 /*----------------------------------Diapazen-----------------------------------*/
 /*----------------------------------ViewsPoll----------------------------------*/
@@ -188,12 +177,6 @@ ORDER BY CHOICE_ID ASC;
 
 
 
-
-
-
-
-
-
 /*-----------------------------------------------------------------------------*/
 /*----------------------insert these data in you database----------------------*/
 /*----------------------------------it's a start-------------------------------*/
@@ -204,11 +187,34 @@ INSERT INTO `diapazen`.`dpz_users` (`id`, `firstname`, `lastname`, `email`, `pas
 VALUES (NULL, 'Adrien', 'Fourel', 'adrien.fourel@isen.fr', '$2a$07$WgHXUqdYPR8A7csxSnhL$.M95uNRXBiMUa3H5YOFBlswm.HFYzaSO', CURRENT_TIMESTAMP, '', NULL);
 
 INSERT INTO `diapazen`.`dpz_users` (`id`, `firstname`, `lastname`, `email`, `password`, `registration_date`, `last_login_date`, `last_login_ip`) 
-VALUES (NULL, 'Nicolas', 'Silvain', 'nicolas.silvain@isen.fr', '$2a$07$SvItH38xYibO6E2eQop/$.tdCCJtGmXxzrJMr5wMbkyVTvR6lUBse', CURRENT_TIMESTAMP, '', NULL);
+VALUES (NULL, 'Nicolas', 'Silvain', 'nicolas.silvain@isen.fr', '$2a$07$9NW3Xe0khGp5tEBzU2RC$.XwWiNlTBAX15h4UXuwf7HnlWL1fuPIi', CURRENT_TIMESTAMP, '', NULL);
 
 INSERT INTO `diapazen`.`dpz_users` (`id`, `firstname`, `lastname`, `email`, `password`, `registration_date`, `last_login_date`, `last_login_ip`) 
 VALUES (NULL, 'Guillaume', 'Bauduin', 'guillaume.bauduin@isen.fr', '$2a$07$rAwN3gqQVFxEvo.8tXMu$.tM0m0MCKxlElNZ09RyrU/2DF2.P3glG', CURRENT_TIMESTAMP, '', NULL);
 
+INSERT INTO `diapazen`.`dpz_users` (`id`, `firstname`, `lastname`, `email`, `password`, `registration_date`, `last_login_date`, `last_login_ip`) 
+VALUES (NULL, 'Anthony', 'Disch', 'anthony.disch@isen.fr', '$2a$07$qK/A.b3sGiR1vNSajpg0$./koNGuL.yYr3NoTNgSeyg6z47fzSjOe', CURRENT_TIMESTAMP, '', NULL);
+
+INSERT INTO `diapazen`.`dpz_users` (`id`, `firstname`, `lastname`, `email`, `password`, `registration_date`, `last_login_date`, `last_login_ip`) 
+VALUES (NULL, 'Julie', 'Albaladejo', 'julie.albaladejo@isen.fr', '$2a$07$SvJt4qnMoWTuUCV3l9bd$.mR98rQsKZF3f1R3.HXf53yiDefTggjS', CURRENT_TIMESTAMP, '', NULL);
+
+INSERT INTO `diapazen`.`dpz_users` (`id`, `firstname`, `lastname`, `email`, `password`, `registration_date`, `last_login_date`, `last_login_ip`) 
+VALUES (NULL, 'Max', 'Destors', 'max.destors@isen.fr', '$2a$07$ycFoXimp2vOZTdWuakqz$.Kdx1G04iAMOi2qpuTAwup60oan2oykO', CURRENT_TIMESTAMP, '', NULL);
+
+INSERT INTO `diapazen`.`dpz_users` (`id`, `firstname`, `lastname`, `email`, `password`, `registration_date`, `last_login_date`, `last_login_ip`) 
+VALUES (NULL, 'Nicolas', 'Nativo', 'nicolas.nativo@isen.fr', '$2a$07$ueBtMPcgnKSYdFTpEwfO$.vHMOfsYeUeoBatSd3UMV8K0sco7iIui', CURRENT_TIMESTAMP, '', NULL);
+
+INSERT INTO `diapazen`.`dpz_users` (`id`, `firstname`, `lastname`, `email`, `password`, `registration_date`, `last_login_date`, `last_login_ip`) 
+VALUES (NULL, 'Jonathan', 'Saez', 'jonathan.saez@isen.fr', '$2a$07$G2H49UyB.gaZwLdSfp1X$.DrPyTsIoMX3mqtGf3HvpE1s86ju3DgO', CURRENT_TIMESTAMP, '', NULL);
+
+INSERT INTO `diapazen`.`dpz_users` (`id`, `firstname`, `lastname`, `email`, `password`, `registration_date`, `last_login_date`, `last_login_ip`) 
+VALUES (NULL, 'Julien', 'Bodet', 'julien.bodet@isen.fr', '$2a$07$nLU2PY3M.i7l8yjfgp9d$.YMCRBKse36wcrmm2mCUcOUIEF7ypS3K', CURRENT_TIMESTAMP, '', NULL);
+
+INSERT INTO `diapazen`.`dpz_users` (`id`, `firstname`, `lastname`, `email`, `password`, `registration_date`, `last_login_date`, `last_login_ip`) 
+VALUES (NULL, 'Timothée', 'Nicolas', 'timothee.nicolas@isen.fr', '$2a$07$Hkp7vAIj/a8qn9SbM.ZK$.vq/bkPpumcXIjpBMHp8eUhRPH8STqHm', CURRENT_TIMESTAMP, '', NULL);
+
+INSERT INTO `diapazen`.`dpz_users` (`id`, `firstname`, `lastname`, `email`, `password`, `registration_date`, `last_login_date`, `last_login_ip`) 
+VALUES (NULL, 'Kenny', 'Barraud', 'kenny.barraud@isen.fr', '$2a$07$n/hOIxYDv0kwbuNeRsoE$.KfZqK3eO9I4FgZq0uHxw.DA5awyAvnW', CURRENT_TIMESTAMP, '', NULL);
 
 /*-----------------------------------------------------------------------------*/
 /*-----------------------------------POLLS-------------------------------------*/
@@ -218,20 +224,29 @@ INSERT INTO `diapazen`.`dpz_polls` (`id`, `user_id`, `url`, `title`, `descriptio
 VALUES (NULL, '2', 'E7DF4FED2ZD', 'Soirée chez moi !', 'bon voila, j''organise une soirée ce samedi, qui voudrai quoi à manger ?', '2013-05-20 14:45:09', '1');
 
 INSERT INTO `diapazen`.`dpz_polls` (`id`, `user_id`, `url`, `title`, `description`, `expiration_date`, `open`) 
-VALUES (NULL, '1', 'MZ7DFG96DD3', 'Voyage cet été', 'bon voila, où part on cet été ?', '2013-06-20 14:45:09', '1');
+VALUES (NULL, '1', 'MZ7DFG96DD3', 'Voyage cet été', 'Où part on cet été ?', '2013-06-20 14:45:09', '1');
 
 INSERT INTO `diapazen`.`dpz_polls` (`id`, `user_id`, `url`, `title`, `description`, `expiration_date`, `open`) 
 VALUES (NULL, '2', 'DFGTR1523S', 'Soirée chez moi !', 'blabla', '2013-05-26 14:45:09', '1');
 
 INSERT INTO `diapazen`.`dpz_polls` (`id`, `user_id`, `url`, `title`, `description`, `expiration_date`, `open`) 
-VALUES (NULL, '2', 'ZER7D4Z5D2', 'Sortie', 'blabla', '2013-05-25 07:28:09', '1');
+VALUES (NULL, '5', 'ZER7D4Z5D2', 'Voyage', 'quelle destination de rêve ?', '2013-05-25 07:28:09', '1');
+
+INSERT INTO `diapazen`.`dpz_polls` (`id`, `user_id`, `url`, `title`, `description`, `expiration_date`, `open`) 
+VALUES (NULL, '8', 'AAAAAAAA', 'RENDEZ VOUS', 'rendez vous du 06/06 pour le projet ..', '2013-05-06 23:59:59', '1');
+
+INSERT INTO `diapazen`.`dpz_polls` (`id`, `user_id`, `url`, `title`, `description`, `expiration_date`, `open`) 
+VALUES (NULL, '9', 'AAAAAAAB', 'RENDEZ VOUS', 'meeting du 22/06 pour le projet ..', '2013-05-21 23:59:59', '1');
+
+/*7*/
+INSERT INTO `diapazen`.`dpz_polls` (`id`, `user_id`, `url`, `title`, `description`, `expiration_date`, `open`) 
+VALUES (NULL, '9', 'BIENTROPBIEN', 'Site', 'Que pensez-vous du site ?', '2013-06-21 23:59:59', '1');
 /*-----------------------------------------------------------------------------*/
 /*---------------------------------CHOICES-------------------------------------*/
 /*-----------------------------------------------------------------------------*/
 INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '1', 'Couscous !');
 INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '1', 'Tajine !');
 INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '1', 'Lasagnes !');
-
 
 INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '2', 'Barcelone !');
 INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '2', 'Paris !');
@@ -245,6 +260,24 @@ INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '
 INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '4', 'Paris ');
 INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '4', 'Berlin !');
 INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '4', 'Londres !');
+
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '5', '8h à 9h');
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '5', '9h à 10h');
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '5', '15h à 16h');
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '5', '18h à 19h');
+
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '6', '8h à 9h');
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '6', '9h à 10h');
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '6', '10h à 11h');
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '6', '11h à 12h');
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '6', '14h à 15h');
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '6', '15h à 16h');
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '6', '16h à 17h');
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '6', '17h à 18h');
+
+/*26*/
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '7', 'Trop bien !');
+INSERT INTO `diapazen`.`dpz_choices` (`id`, `poll_id`, `choice`) VALUES (NULL, '7', 'Génial !');
 
 /*-----------------------------------------------------------------------------*/
 /*---------------------------------RESULTS-------------------------------------*/
@@ -264,3 +297,39 @@ INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, 
 
 INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '12', 'Pierre');
 INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '12', 'Eric');
+
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '14', 'Pierre');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '14', 'Eric');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '15', 'Jean');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '15', 'Eric');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '15', 'Jean');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '16', 'Arthur');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '17', 'Pierre');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '17', 'Eric');
+
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '18', 'Pierre');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '18', 'Eric');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '18', 'Jean');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '18', 'Arthur');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '19', 'Jean');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '20', 'Arthur');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '20', 'Pierre');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '21', 'Eric');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '21', 'Jean');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '21', 'Eric');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '21', 'Jean');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '25', 'Arthur');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '25', 'Pierre');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '25', 'Eric');
+
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '26', 'Anthony');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '26', 'Timothée');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '26', 'Julie');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '26', 'Adrien');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '26', 'Nicola');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '26', 'Nicola');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '26', 'Kennu');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '27', 'Jonathan');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '27', 'Guillaume');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '27', 'Max');
+INSERT INTO `diapazen`.`dpz_results` (`id`, `choice_id`, `value`) VALUES (NULL, '27', 'Julien');
