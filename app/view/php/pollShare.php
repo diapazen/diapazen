caa5ef3ff33e189999315d03c63f71c478f39356
Max Destors <max.destors@gmail.com>
2013-05-15 16:22:01 +0200

    Modification vue creation de sondage /partage
    Ajout d'un bouton 'retour' et suppression d'un text.

--- app/view/php/pollShare.php
+++ app/view/php/pollShare.php
@@ -5,11 +5,11 @@
             <form id="share_form" class="default_form" method="post" action="sharePoll">
                 <h2 class="title">Votre sondage a bien été créé !</h2>
                 <h3 class="small_title">Lien du sondage</h3>
-                <p class="orange_text" >Copier ce lien pour le donner à qui vous souhaitez.</p>
                 <input type="text" class="text_edit" onClick="this.select();" value="localhost<?php $this->getHomeUrl(); ?>/poll/view/<?php echo $pollUrl; ?>" />
                 <h3 class="small_title">Partagez ce lien par e-mail</h3>
                 <textarea class="text_edit" name="mails" placeholder="mail.example@mail.com"></textarea>
                 <input class="orange_button" type="submit" value="Partager">
+                <a class="orange_button" href="<?php $this->getHomeUrl(); ?>">Retour</a>
             </form>
         </div>
 
