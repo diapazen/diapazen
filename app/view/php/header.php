0d248346b99a67345c4fde00d62aa7a44f7b6274
Max Destors <max.destors@gmail.com>
2013-05-15 15:20:07 +0200

    Correction du chemin d'acces de JQuery

--- app/view/php/header.php
+++ app/view/php/header.php
@@ -7,7 +7,7 @@
         <link rel="stylesheet" type="text/css" href="<?php $this->getPath('css/diapazen.css'); ?>">
         <link rel="stylesheet" type="text/css" href="<?php $this->getPath('css/style.css'); ?>">
         <!-- jQuery 1.9-->
-        <script src="../js/jquery.js"> </script>
+        <script src="<?php $this->getPath('js/jquery.js'); ?>"> </script>
         
         <title><?php if (isset($title)){ echo $title; } ?></title>
     </head>
