4928ae2d7140a91cd976622def91abfa38784c00
Julien Bodet <julien.bodet92@gmail.com>
2013-05-23 12:03:57 +0200

    Modifications css formulaires + ajout de label vue login

--- app/view/php/login.php
+++ app/view/php/login.php
@@ -22,8 +22,10 @@
             
             <form action="<?php $this->getHomeUrl(); ?>/user/login" class="default_form" id="connect_box_fail" method="post">
             	<p class="title" >Réessayer de vous connecter !</p>
+                <label for="mail_connect_fail" class="text" >E-mail</label>
 	        	<input id="mail_connect_fail" name="mailConnect" class="text_edit" type="mail" placeholder="E-mail">
-	        	<input id="password_connect_fail" name="passwordConnect" class="text_edit" type="password" placeholder="Mot de passe">
+	        	<label for="password_connect_fail" class="text" >Mot de passe</label>
+                <input id="password_connect_fail" name="passwordConnect" class="text_edit" type="password" placeholder="Mot de passe">
 	        	<input class ="orange_button" type="submit" value="Connexion">
             </form>
         </div>
