94d358b7a1717e425cec035a59db42466b7e473e
Timothée Nicolas <timothnicolas@gmail.com>
2013-05-25 12:54:37 +0200

    Ajout de la page À propos

---
+++ app/view/php/about.php
@@ -0,0 +1,9 @@
+<?php $this->getHeader(); ?>
+        
+        <div id="content">
+            
+            <div>TODO: A remplir cette page</div>
+
+        </div>
+
+<?php $this->getFooter(); ?>
--- app/view/php/footer.php
+++ app/view/php/footer.php
@@ -2,8 +2,8 @@
 		<footer>
 		    <ul>
 		        <li><a class="link" href="<?php $this->getHomeUrl(); ?>">Accueil</a></li>
-		        <li><a class="link" href="#">À propos</a></li>
-		        <li><a class="link" href="#">Contact</a></li>
+		        <li><a class="link" href="<?php $this->getHomeUrl(); ?>/about">À propos</a></li>
+		        <!-- <li><a class="link" href="#">Contact</a></li> -->
 		    </ul>
 		    <p class="text">© <?php echo date('Y'); ?> Diapazen</p>
 		</footer>
