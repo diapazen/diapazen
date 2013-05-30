874ee0929d8d97b5a138705867381835d226b877
Julien Bodet <julien.bodet92@gmail.com>
2013-05-30 10:06:20 +0200

    Ajout labels vue Partage

--- app/view/php/pollShare.php
+++ app/view/php/pollShare.php
@@ -5,8 +5,10 @@
             <form id="share_form" class="default_form" method="post" action="sharePoll">
                 <h2 class="title">Votre sondage a bien été créé !</h2>
                 <h3 class="small_title">Lien du sondage</h3>
+                <label class="text" >Lien</label>
                 <input type="text" class="text_edit" onClick="this.select();" value="localhost<?php $this->getHomeUrl(); ?>/p/<?php echo $pollUrl; ?>" />
                 <h3 class="small_title">Partagez ce lien par e-mail</h3>
+                <label class="text">E-mails</label>
                 <textarea class="text_edit" name="mails" placeholder="mail.example@mail.com"></textarea>
                 <input class="orange_button" type="submit" value="Partager">
                 <a class="orange_button" href="<?php $this->getHomeUrl(); ?>">Retour</a>
