6768026c68d3dde16920d0575b077b0340fea31d
Max Destors <max.destors@gmail.com>
2013-05-31 19:01:13 +0200

    Readonly sur le lien de partage

--- app/view/php/pollShare.php
+++ app/view/php/pollShare.php
@@ -6,7 +6,7 @@
                 <h2 class="title">Votre sondage a bien été créé !</h2>
                 <h3 class="small_title">Lien du sondage</h3>
                 <label class="text" for="poll_link" >Lien</label>
-                <input type="text" id="poll_link" class="text_edit" onClick="this.select();" value="<?php echo $this->getHomeUrl().'/p/'.$pollUrl; ?>" />
+                <input type="text" id="poll_link" class="text_edit" readonly onClick="this.select();" value="<?php echo $this->getHomeUrl().'/p/'.$pollUrl; ?>" />
                 <h3 class="small_title">Partagez ce lien par e-mail</h3>
                 <label class="text lbl_textarea" for="mails">E-mails</label>
                 <textarea class="text_edit" id="mails" name="mails" placeholder="mail.example@mail.com"></textarea>
