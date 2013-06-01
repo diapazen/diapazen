8d43006f615d625370468c7017ca55d9ff105969
Timothée Nicolas <timothnicolas@gmail.com>
2013-06-01 15:48:53 +0200

    Modification de la page À propos

--- app/view/php/about.php
+++ app/view/php/about.php
@@ -1,9 +1,11 @@
 <?php $this->getHeader(); ?>
         
         <div id="content">
-            
-            <div>TODO: A remplir cette page</div>
-
+            <p class="title">À propos de Diapazen</p>
+            <br>
+            <p class="text">Diapazen permet de planifier rapidement des événements avec ses collaborateurs.</p>
+            <br>
+            <p class="text">C'est un service libre et gratuit réalisé par des étudiants de l'<a class="link" target="_blank" href="http://www.isen.fr/toulon.asp">ISEN-Toulon</a>. Le code source est disponible sur <a class="link" target="_blank" href="http://github.com/diapazen/diapazen">github.com/diapazen</a>.</p>
         </div>
 
 <?php $this->getFooter(); ?>
