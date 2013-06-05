e5b0014f5f75f345954dc0425f1155f91fec7e4b
Julien Bodet <julien.bodet92@gmail.com>
2013-06-05 18:57:30 +0200

    Ajout astérisque sur champs obligatoires

--- app/view/php/forgot.php
+++ app/view/php/forgot.php
@@ -19,7 +19,7 @@
             ?>  
             <form onsubmit="return formCheck(this)" id="form_forgot" action="<?php $this->getHomeUrl(); ?>/user/forgot" class="default_form" method="post">
             	<p class="orange_text info_box" >Entrez votre e-mail pour reçevoir un nouveau mot de passe.</p>
-	        	<label for="mailRetrieve" class="text">E-mail</label>
+	        	<label for="mailRetrieve" class="text">E-mail<span class="asterisc"> *</span></label>
 	        	<input type="mail" id="mailRetrieve" name="email" class="text_edit" >
 	        	<input class ="orange_button" type="submit" value="Envoyer">
             </form>
--- app/view/php/personalData.php
+++ app/view/php/personalData.php
@@ -18,11 +18,11 @@
         ?>
         <form onsubmit="return formCheck(this)" id="personal_data_form" class="default_form" action="profile" method="post">
             <p class="big_title" id="title_personnal_data">Données personnelles</p>
-            <label class="text" for="user_lastname">Nom</label>
+            <label class="text" for="user_lastname">Nom<span class="asterisc"> *</span></label>
             <input type="text" id="user_lastname" class="text_edit" value="<?php if(isset($lastname)){echo $lastname;}?>" name="lastNameUser">
-            <label class="text" for="user_firstname">Prénom</label>
+            <label class="text" for="user_firstname">Prénom<span class="asterisc"> *</span></label>
             <input type="text" id="user_firstname" class="text_edit" value="<?php if(isset($firstname)){echo $firstname;}?>" name="firstNameUser">
-            <label class="text" for="user_mail">E-mail</label>
+            <label class="text" for="user_mail">E-mail<span class="asterisc"> *</span></label>
             <input id="user_mail" type="mail" class="text_edit" value="<?php if(isset($email)){echo $email;}?>" name="email">
             <label class="text" for="new_pwd">Nouveau mot de passe</label>
             <input type="password" id="new_pwd" class="text_edit" value="" name="newPassword">
@@ -30,7 +30,7 @@
             <input type="password" id="new_pwd_confirm" class="text_edit" value="" name="passwordConfirm">
             <br/>
             <p class="orange_text info_box">Pour confirmer les modifications, entrez votre mot de passe actuel.</p>
-            <label class="text" for="pwd_user">Mot de passe</label>
+            <label class="text" for="pwd_user">Mot de passe<span class="asterisc"> *</span></label>
             <input type="password" id="pwd_user" class="text_edit" value="" name="password">
             <a class="grey_button" href="<?php $this->getHomeUrl(); ?>/dashboard" >Annuler</a>
             <input class="orange_button" type="submit" value="Modifier">
